02a89798a52708c7ffea5c3367df0ecf1f16a0c5
Andrew Ackah-Miezan <ack@aam.fr>
2021-12-23 23:28:30 +0100

    second

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿const title = React.createElement('h1',{},
+    'hello all ','Jean ',
+    React.createElement('span',{},10))
+console.log(title)
+
+ReactDOM.render(title,document.querySelector('#app'))
+
+// document.querySelector('#app').innerHTML = '<h1>Bonjour tout le monde</h1>'
